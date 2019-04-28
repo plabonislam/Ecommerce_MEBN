95489939794050671446b38a56e25a875c8aafd5
plabonislam <shahnurislamplabon@gmail.com>
2019-04-28 17:36:37 +0600

    out put from seed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Product=require('../models/product');
 var mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/shop', {useNewUrlParser: true});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  //want to grab seed data from data base...as asynchronise data retriving
+  Product.find(function(err,docs)
+  {
+    var chunk=[];
+    var chunksize=3;
+    for(var i=0;i<docs.length;i+=chunksize)
+    {
+      chunk.push(docs.slice(i,i+chunksize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart',products: chunk });
+  });
+  
 });
 
 module.exports = router;
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -11,21 +11,21 @@ var products=[new Product(
 }),
 new Product(
     {
-     imagePath:'https://static1.squarespace.com/static/52fc05c9e4b08fc45bd99090/t/5ca392fbe4966bd365870db7/1554223873227/mgot-aftermath-poster.jpg',
+     imagePath:'https://i.pinimg.com/originals/00/f3/71/00f371b879192e6f0a52c12d1f03b214.jpg',
      title:'game of thrones',
      description:'song of ice and fire ',
      price:10
     }),
     new Product(
         {
-         imagePath:'https://static1.squarespace.com/static/52fc05c9e4b08fc45bd99090/t/5ca392fbe4966bd365870db7/1554223873227/mgot-aftermath-poster.jpg',
+         imagePath:'http://hdqwalls.com/wallpapers/dragon-night-king-game-of-thrones-season-8-5s.jpg',
          title:'game of thrones',
          description:'song of ice and fire ',
          price:10
         }),
         new Product(
             {
-             imagePath:'https://static1.squarespace.com/static/52fc05c9e4b08fc45bd99090/t/5ca392fbe4966bd365870db7/1554223873227/mgot-aftermath-poster.jpg',
+             imagePath:'https://wallpaperaccess.com/full/529281.jpg',
              title:'game of thrones',
              description:'song of ice and fire ',
              price:10
